bd8e1c98058d4322dc044a4573c99e9bcacf3ece
hgokhru101 <54789722+hgokhru101@users.noreply.github.com>
2019-09-10 04:26:47 -0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+function openSM(){
+	document.getElementById("mySidemenu").style.width="450px";
+	document.getElementById("pg-content").style.marginLeft="450px";
+}
+function closeSM(){
+	document.getElementById("mySidemenu").style.width="0";
+	document.getElementById("pg-content").style.marginLeft="0";
+}

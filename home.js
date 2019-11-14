dfaf1926fbbc984868b7c06bab339498593d800b
hgokhru101 <54789722+hgokhru101@users.noreply.github.com>
2019-11-14 18:23:05 +0530

    Delete home.js

--- home.js
+++
@@ -1,8 +0,0 @@
-function openSM(){
-	document.getElementById("mySidemenu").style.width="450px";
-	document.getElementById("pg-content").style.marginLeft="450px";
-}
-function closeSM(){
-	document.getElementById("mySidemenu").style.width="0";
-	document.getElementById("pg-content").style.marginLeft="0";
-}
